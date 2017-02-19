34753f0c013231148b3fb5fa0681ffa0bda73930
Hakan Cem <cemkulcu@msn.com>
2017-02-19 21:07:01 +0200

    admin

--- upload/index.php
+++ upload/index.php
@@ -9,11 +9,11 @@ if (is_file('config.php')) {
 
 // Install
 if (!defined('DIR_APPLICATION')) {
-	header('Location: install/index.php');
+	header('Location: ../install/index.php');
 	exit;
 }
 
 // Startup
 require_once(DIR_SYSTEM . 'startup.php');
 
-start('catalog');
+start('admin');
